87721539121d8b740244e4fb9396b2f529b03789
Scott <marden.scott@gmail.com>
2017-06-09 15:33:28 -0700

    done with optimizations, cut down on variable usage.

--- server.py
+++ server.py
@@ -10,49 +10,30 @@ def index():
 		print session['num']
 	if 'response' not in session:
 		session['response'] = ""
-	if 'marker' not in session:
-		session['marker'] = ""
-	if 'guessing' not in session:
-		session['guessing'] = "visible"
-	if 'reseting' not in session:
-		session['reseting'] = "invisible"
-	return render_template('index.html', num=session['num'], response=session['response'], marker=session['marker'], guess_class=session['guessing'], reset_class=session['reseting'])
+	return render_template('index.html', num=session['num'], response=session['response'])
 
 @app.route('/guess', methods=['POST'])
 def guess():
 	guess = int(request.form['guess'])
 	if guess == session['num']:
 		#success
-		print "Got it!"
+		# print "Got it!"
 		session['response'] = str(session['num']) + "  was the number!"
-		session['marker'] = "box_right"
-		session['guessing'] = "invisible"
-		session['reseting'] = "visible"
 	elif guess > session['num']:
 		#too high!
-		print "Too high!"
+		# print "Too high!"
 		session['response'] = "Too high!"
-		session['marker'] = "box_wrong"
 	elif guess < session['num']:
 		#too low!
-		print "Too low!"
+		# print "Too low!"
 		session['response'] = "Too low!"
-		session['marker'] = "box_wrong"
 	return redirect('/')
 
 @app.route('/reset', methods=['POST'])
 def reset():
 	session.pop('num')
-	session.pop('marker')
 	session.pop('response')
-	session.pop('guessing')
-	session.pop('reseting')
-
-
 	return redirect('/')
-
-
-
 
 
 app.run(debug=True)
